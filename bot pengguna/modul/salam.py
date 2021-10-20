ce2fa9cccfef31a3bc2a5d2499ff4fa670e618d7
apiss27 <92074104+Hafisnuranzari@users.noreply.github.com>
2021-10-21 04:49:41 +0700

    Update and rename userbot/modules/salam.py to bot pengguna/modul/salam.py

--- bot pengguna/modul/salam.py
+++ bot pengguna/modul/salam.py
@@ -16,13 +16,13 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb.`")
+    await typew.edit("`Assalamualaikum sayang`")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb.`")
+    await typew.edit("`waalaikumsalam sayang.`")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')

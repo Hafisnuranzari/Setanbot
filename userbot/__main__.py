bc1355195d79ac869040cd688ef405a043095363
apiss27 <92074104+Hafisnuranzari@users.noreply.github.com>
2021-12-02 22:02:26 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡Geez - Project⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"⚡GHOIB UBOT⚡ ⚙️ V{BOT_VER} [POCONG DAN KAWAN KAWAN TELAH DIBANGKITKAN DARI KUBUR UNTUK MENEMANI KAMU TIDUR!]")
 
 
 if len(argv) not in (1, 3, 4):
